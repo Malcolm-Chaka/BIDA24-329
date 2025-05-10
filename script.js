be83bbd597e1a5673753ad362bfb4579f62a783d
Malcolm-Chaka <bida24-329@thuto.bac.ac.bw>
2025-05-10 21:13:12 +0200

    Video carousel update

--- script.js
+++ script.js
@@ -6,18 +6,3 @@ navLinks.forEach(link => {
     link.classList.add('active');
   }
 });
-
-document.addEventListener("DOMContentLoaded", function () {
-  const carousel = document.querySelector("#videoCarousel");
-  const carouselInstance = bootstrap.Carousel.getOrCreateInstance(carousel);
-  const videos = carousel.querySelectorAll("video");
-
-  videos.forEach((video, index) => {
-    video.addEventListener("ended", function () {
-      // Only advance if this is NOT the last video
-      if (index < videos.length - 1) {
-        carouselInstance.next();
-      }
-    });
-  });
-});
