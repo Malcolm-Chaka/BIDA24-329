fc8d31585d3f47ed3f3bd607495b33e274e03c3d
Malcolm-Chaka <bida24-329@thuto.bac.ac.bw>
2025-05-11 15:33:13 +0200

    Update Javascript

--- script.js
+++ script.js
@@ -1,26 +1,3 @@
-
-
-//
-document.addEventListener("DOMContentLoaded", function() {
-  const urlParams = new URLSearchParams(window.location.search);
-
-  const name = urlParams.get("name");
-  const description = urlParams.get("description");
-  const price = urlParams.get("price");
-  const image = urlParams.get("image");
-
-  document.getElementById("productName").textContent = name;
-  document.getElementById("productDescription").textContent = description;
-  document.getElementById("productPrice").textContent = price;
-  document.getElementById("productImage").src = image;
-
-  // Update Buy Now button link
-  const buyNowBtn = document.getElementById("buyNowBtn");
-  const encodedImage = encodeURIComponent(image);
-  const encodedName = encodeURIComponent(name);
-  buyNowBtn.href = `checkout.html?name=${encodedName}&price=${price}&image=${encodedImage}`;
-});
-
 // Highlight the current page's nav link
 const navLinks = document.querySelectorAll('.nav-link');
 const currentUrl = window.location.href;
